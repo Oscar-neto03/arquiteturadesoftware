0f23a9dda6fe296c55657e5f35eefebbcee319f3
Oscar-neto03 <87312830+Oscar-neto03@users.noreply.github.com>
2021-07-12 10:02:34 -0300

    Add files via upload

---
+++ INSERTINTO-PIXAR.sql
@@ -0,0 +1,7 @@
+USE pixar; 
+
+INSERT INTO movies (title, director, year, length_minutes)
+VALUES  ('Monstros SA', 'Peter Docter', '2001', 92),
+		('Procurando Nemo', 'Jhon Lasseter', 2003, 107),
+		('Os Incríveis', 'Brad Bird', 2004, 116),
+		('Wall-E', 'Pete Docter', 2008, 104);
---
+++ sql10.sql
@@ -0,0 +1,8 @@
+SELECT id FROM movies
+WHERE director = 'Andrew Station'; 
+
+DELETE FROM boxoffice
+WHERE movie_id IN (1, 9);
+
+DELETE FROM movies 
+WHERE director = 'Andrew Station'; 
